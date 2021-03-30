e2b2b2554657077f5dd5d68bf09fbebacc855f6c
Luis Araya Mata <luistec2309@estudiantec.cr>
2021-03-30 11:43:47 -0600

    Prueba de conexion exitosa, comenzar interfaces

--- AdministradorPlanillaObrera/src/java/modelo/conexionBD.java
+++ AdministradorPlanillaObrera/src/java/modelo/conexionBD.java
@@ -1,23 +1,33 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package modelo;
+
+import javax.inject.Named;
+import javax.enterprise.context.ApplicationScoped;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.DatabaseMetaData;
 
+
+@Named(value = "conexionBD")
+@ApplicationScoped
 public class conexionBD {
-    
-    public static void main(String[] args) {
-            String conexionUrl = "jdbc:sqlserver://localhost:1433;databaseName=PlanillaObrera;integratedSecurity=true;";
+    public static Connection getConexion() {
+            String conexionUrl = "jdbc:sqlserver://localhost:1433;databaseName=PlanillaObrera;user=sa;password=admin";
             try{
                 Connection con =  DriverManager.getConnection(conexionUrl);
                 System.out.println("Conexion lista");
+                return con;
             } catch(SQLException ex){
                 System.out.println(ex.toString());
-                
+                return null;
             }
         }
+    
+    public conexionBD() {
+    }
     
 }
